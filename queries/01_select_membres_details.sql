ec1a6edc88a74e30991af4fb8e303c0111f906db
Sebcoach13 <140277344+Sebcoach13@users.noreply.github.com>
2026-05-25 15:45:04 +0200

    feat: ajout de la premiere requete de jointure des membres

---
+++ queries/01_select_membres_details.sql
@@ -0,0 +1,8 @@
+SELECT
+    m.prenom,
+    m.nom,
+    s.nom AS nom_desalle,
+    a.type_abonnement AS formule
+FROM membres AS m
+INNER JOIN salles AS s ON m.salle_id = s.id
+INNER JOIN abonnements AS a ON m.abonnement_id = a.id;
