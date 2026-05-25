fd229aac0b99211dd88df5afcac7e8ea3779c5ea
Sebcoach13 <140277344+Sebcoach13@users.noreply.github.com>
2026-05-25 22:23:29 +0200

    fix: alignement correct du fichier yaml

--- schema/02_init_abonnements.sql
+++ schema/02_init_abonnements.sql
@@ -4,6 +4,8 @@ CREATE TABLE abonnements (
     prix_mensuel DECIMAL(5, 2) NOT NULL
 );
 
-INSERT INTO abonnements (type_abonnement, prix_mensuel) VALUES
+INSERT INTO abonnements (type_abonnement, prix_mensuel)
+VALUES
     ('Forme', 30.00),
     ('Premium', 50.00);
+    
--- schema/03_init_membres.sql
+++ schema/03_init_membres.sql
@@ -4,18 +4,12 @@ CREATE TABLE membres (
     prenom VARCHAR(100) NOT NULL,
     email VARCHAR(150) UNIQUE NOT NULL,
     date_inscription DATE NOT NULL,
-    salle_id INT REFERENCES salles(id),
-    abonnement_id INT REFERENCES abonnements(id)
+    salle_id INT REFERENCES salles (id),
+    abonnement_id INT REFERENCES abonnements (id)
 );
 
-INSERT INTO membres (
-    nom,
-    prenom,
-    email,
-    date_inscription,
-    salle_id,
-    abonnement_id
-) VALUES
+INSERT INTO membres (nom, prenom, email, date_inscription, salle_id, abonnement_id)
+VALUES
     ('Dupont', 'Jean', 'jean.dupont@email.com', '2026-01-15', 1, 1),
     ('Martin', 'Alice', 'alice.martin@email.com', '2026-02-20', 2, 2),
     ('Rousseau', 'Marc', 'marc.rousseau@email.com', '2026-03-05', 1, 2);
